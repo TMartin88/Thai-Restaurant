7525c309bb32b237be6f76f09c04050d99391a78
TMartin88 <tiernanmartin1@gmail.com>
2023-01-21 22:46:21 +0000

    Change heroku App

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["tablebooking.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["timetablemanager.herokuapp.com/", "localhost"]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
